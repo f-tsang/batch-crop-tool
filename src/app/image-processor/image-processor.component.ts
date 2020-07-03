2766108782fb25b27b0432a1fa1ff6569f649249
Frederick Tsang <frederick.tsang@outlook.com>
2020-07-02 18:31:39 -0700

    Implemented zipped download.

--- src/app/image-processor/image-processor.component.ts
+++ src/app/image-processor/image-processor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { saveAs } from 'file-saver'
+import * as JSZip from 'jszip'
+import { Observable } from 'rxjs'
+import { mergeAll, mergeMap, reduce, take } from 'rxjs/operators'
 
 /**
  * TODO - ...
@@ -6,15 +10,51 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-image-processor',
   template: `
-    <header>4 - Finish</header>
-    <a download="batch-crop-tool" color="primary" mat-flat-button
-      >Download images (.zip)</a
-    >
+    <mat-form-field>
+      <mat-label>Filename</mat-label>
+      <input [(value)]="filename" matInput />
+    </mat-form-field>
+    <button color="primary" (click)="downloadZippedImages()" mat-flat-button>
+      Download images (.zip)
+    </button>
   `,
-  styles: []
+  styles: [
+    `
+      :host {
+        display: grid;
+        grid-auto-flow: row;
+        justify-content: left;
+      }
+      header {
+        margin-bottom: 1rem;
+      }
+    `
+  ]
 })
-export class ImageProcessorComponent implements OnInit {
-  constructor() {}
+export class ImageProcessorComponent {
+  @Input()
+  images: Observable<string[]>
+  filename = this.defaultFilename
+  extension = '.zip'
 
-  ngOnInit(): void {}
+  downloadZippedImages() {
+    const filename = this.filename === '' ? this.defaultFilename : this.filename
+    const addImageToZip = (zipFile: JSZip, image: string, index: number) => {
+      const base64Image = image.split(',')[1]
+      zipFile.file(`${index}.png`, base64Image, { base64: true })
+      return zipFile
+    }
+    this.images
+      .pipe(
+        take(1),
+        mergeAll(),
+        reduce(addImageToZip, new JSZip()),
+        mergeMap(zipFile => zipFile.generateAsync({ type: 'blob' }))
+      )
+      .subscribe(zipFile => saveAs(zipFile, `${filename}${this.extension}`))
+  }
+
+  get defaultFilename() {
+    return 'batch-crop-tool'
+  }
 }
--- src/app/image-processor/image-processor.module.ts
+++ src/app/image-processor/image-processor.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 
 import { ImageProcessorComponent } from './image-processor.component'
 
 @NgModule({
   declarations: [ImageProcessorComponent],
   exports: [ImageProcessorComponent],
-  imports: [CommonModule, MatIconModule, MatButtonModule]
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatInputModule]
 })
 export class ImageProcessorModule {}
